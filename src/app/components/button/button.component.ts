fa1032b52851d53701754bed097712ffbdaf3b5f
LengyHELL <ubi19998@gmail.com>
2022-05-25 19:58:18 +0200

    Adding counter

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() text!: string;
+  @Input() disabled: boolean = false;
   @Output() btnClick = new EventEmitter();
 
   constructor() { }
